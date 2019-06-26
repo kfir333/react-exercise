b13735847214664180e85a6e2c5b44e39793445f
kfir <kfir333@gmail.com>
2019-06-26 10:33:55 +0300

    add star 3

---
+++ d3_props/star - ex3/logic.js
@@ -0,0 +1,25 @@
+class Box extends React.Component {
+  render() {
+    return <div className={`box ${this.props.boxSize}`} />;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="boxesOnTop">
+          <Box boxSize="x-small" />
+          <Box boxSize="small" />
+          <Box boxSize="medium" />
+          <Box boxSize="small" />
+          <Box boxSize="x-small no-margin-left " />
+          <Box boxSize="large" />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
